a0b6ce984bb211c1813df5113e95d0a4c8180c56
dinnu93 <dinesh.padisetti@gmail.com>
2016-01-26 20:12:52 +0530

    Added the parseINI function

--- intro.js
+++ intro.js
@@ -527,3 +527,27 @@ function removeSingleQuotes(s){
     return s.replace(regExp, '"');
 }
 
+// Programming Language : Egg
+
+function parseINI(s){
+    var currentSection = {name: null, fields: []};
+    var categories = [currentSection];
+
+    s.split(/\r?\n/).forEach(function(line){
+	var match;
+	if(/^\s*(;.*)?$/.test(line)){
+	    return;
+	}else if(match = line.match(/^\[(.*)\]$/)){
+	    currentSection = {name: match[1], fields: []};
+	    categories.push(currentSection);
+	}else if(match = line.match(/^(\w+)=(.*)$/)){
+	    currentSection.fields.push({name: match[1],
+					value: match[2]});
+	}else{
+	    throw new Error("Line '" + line + "' is invalid.");
+	}
+    });
+
+    return categories;
+}
+
